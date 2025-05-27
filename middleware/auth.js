9b4116852b05f9303bcfcfeb28f25b351f7ee703
Dr. Opeyemi Fasanu <94376218+yemyemco@users.noreply.github.com>
2025-05-27 05:05:39 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,33 @@
+const jwt = require("jsonwebtoken");
+const userdb = require("../userDB");
+const config = process.env;
 
+//Verify user's token   
+const verifyToken = async (req, res, next)=>
+{
+    try {
+    let getToken = req.headers["x-access-token"] || req.header("Authorisation") || req.body.token || req.query.token;
+
+    if(!getToken)
+    {
+        return res.status(400).json({Message: "Token error: Log in again"});
+    }
+        
+        //Verify token
+        getToken = jwt.verify(getToken, config.ACCESS_TOKEN);
+
+        return next();
+
+    } catch (error) {
+        if(error.message == "jwt expired")
+        {
+            return res.status(400).json({Message: "Session expired. Please log in again"});
+        }
+        else
+        {
+            return res.status(400).json({Message: "Encountered error: " + error.message});
+        }
+    }
+}    
+
+module.exports = verifyToken;
