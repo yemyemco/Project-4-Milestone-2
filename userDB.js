0e40f483c813e705bbfb42de659ddeabf9bfb274
Dr. Opeyemi Fasanu <94376218+yemyemco@users.noreply.github.com>
2025-05-27 04:45:30 +0100

    Update userDB.js

--- userDB.js
+++ userDB.js
@@ -4,14 +4,13 @@ const userSchema = new mongoose.Schema(
     { 
         firstName: {type: String, require: true},
         lastName: {type: String, require: true},
-        email: {type: String, unique: true, require: true},
+        email: {type: String, require: true},
         pass: {type: String, require: true},
-        role: {type: String, default: "student"}
+        role: {type: String, default: "student"},
+        token: {type: String}
     },
     {timestamps: true}
 );
 
 const userDB = new mongoose.model("userDB", userSchema);
 module.exports = userDB;
-
-
